c76e69faf06bf9fedbb12b170717c08457e7ab62
Able <ablekilinda@gmail.com>
2022-10-28 00:16:27 +0300

    lotan

--- admin/includes/function.php
+++ admin/includes/function.php
@@ -1,7 +1,8 @@
 <?php
      
      require_once 'config.php';
-     
+
+     //get user roles
 function getUserAccessRoleByID($id)
 	{
 		global $conn;
@@ -13,6 +14,25 @@ function getUserAccessRoleByID($id)
 		
 		return $row['user_role'];
 	}
+
+
+	//get topic
+function getTopic(){
+	global $conn;
+
+	$query = "select * from topic_tbl";
+	$result = $conn->query($query);
+	if ($result -> num_rows>0){
+		while($row = $result->fetch_assoc()){
+       echo $row["topic_name"];
+		}
+	}else {
+		echo "0 Topics";
+	}
+
+	$conn->close();
+
+}
 
 
 ?>
--- inc/database.sql
+++ inc/database.sql
@@ -55,3 +55,13 @@ INSERT INTO `tbl_user_role` (`id`, `user_role`) VALUES
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+
+CREATE TABLE IF NOT EXISTS `topic_tbl` {
+  `topic_id` int NOT NULL AUTO_INCREMENT,
+  `topic_name` varchar DEFAULT NULL,
+  `topic_desc` varchar DEFAULT NULL,
+  PRIMARY KEY (`id`)
+} ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+INSERT INTO `topic-tbl` (`topic_id`, `topic_name`, `topic_desc`) VALUES
+  (1,'Taita', 'talks about taita people');
