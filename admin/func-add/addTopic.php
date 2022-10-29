65d6d3b5ea97428b19414cf85ab2841822060f5a
Able <ablekilinda@gmail.com>
2022-10-29 07:23:50 +0300

    addpost

--- admin/func-add/addTopic.php
+++ admin/func-add/addTopic.php
@@ -1,26 +1,22 @@
 <?php
-   
-   require_once '../includes/config.php';
-function addTopic(){
-	global $conn;
 
-	if (isset($_POST['add-btn'])){
-
-		
-		$topic = $_POST['topics'];
-		$topd = $_POST['topdes'];
-
-		$sql= "insert into topic_tbl (topic_name, topic_desc) values($topic, $topd)";
-		if($conn->query($sql)=== true){
-			echo "Topic Inserted Successfully";
-		}else {
-			echo "Error".$conn->error;
-		}
-
-	}
-	
-	$conn->close();
+if (isset($_POST['add-btn'])){	
+$topic = $_POST['topics'];
+$topd = $_POST['topdes'];
+addTopic($topic,$topd);
 
 }
 
-addTopic();
+
+function addTopic($topic,$topd){
+	global $conn;
+
+	$sql= "insert into topic_tbl (topic_name, topic_desc) values($topic, $topd)";
+	if($conn->query($sql)=== true){
+		echo "Topic Inserted Successfully";
+	}else {
+		echo "Error".$conn->error;
+	}
+	$conn->close();
+
+}
