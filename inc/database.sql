ab8ad1fa1f37d1639bf19bc45bd8b9f5dbcafbd4
Able <ablekilinda@gmail.com>
2022-10-30 08:32:02 +0300

    lotan

--- inc/database.sql
+++ inc/database.sql
@@ -65,3 +65,24 @@ CREATE TABLE IF NOT EXISTS `topic_tbl` (
 
 INSERT INTO `topic_tbl` (`topic_id`, `topic_name`, `topic_desc`) VALUES
   (1,'Taita', 'talks about taita people');
+
+
+  CREATE TABLE IF NOT EXISTS `posts` {
+    `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY;
+    `user_id` int(11) DEFAULT NULL,
+    `title` varchar(255) NOt NULL,
+    `slug` varchar(255) NOT NULL,
+    `view` int(11) NOT NULL,
+    `image` varchar(255) NOT NULL,
+    `body` text NOT NULL,
+    `published` tinyint(1) NOT NULL,
+    `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `updated_at` timestamp NOT NULL '0000-00-00 00:00:00',
+    FOREIGN KEY (`user_id`) REFERENCES `tbl_users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+}ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `post_topic` {
+  `id` int(11),
+  `post_id` int(11),
+  `topic_id` int(11)
+}
